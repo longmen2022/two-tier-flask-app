b86db23d78bb6289ceeac1e7e3ee259b2202ff35
longmen2022 <98563918+longmen2022@users.noreply.github.com>
2025-02-24 12:25:16 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbooktwotier" # Replace with your actual S3 bucket name
-    key    = "EKS-Ajay/terraform.tfstate"
+    bucket = "longmen-sslbucket" # Replace with your actual S3 bucket name
+    key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
 }
